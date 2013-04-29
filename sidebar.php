8ff2cf86a36ce1308bbcb2c708a7df314ff4de68
Math Orientation <mathorient@csclub.uwaterloo.ca>
2013-04-29 04:53:50 -0400

    Temporary sidebar link fix

--- sidebar.php
+++ sidebar.php
@@ -1,21 +1,7 @@
 				<div id="sidebar1" class="fluid-sidebar sidebar span4" role="complementary">
 				
 
-          <?php
-
-            if($post->post_parent)
-              $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
-            else
-              $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
-              if ($children) { ?>
-                <ul>
-                <?php echo $children; ?>
-                </ul>
-              <?php } ?>
-
-
-
-						<?php dynamic_sidebar( 'sidebar1' ); ?>
+					<?php dynamic_sidebar( 'sidebar1' ); ?>
 
 					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 					<?php else : ?>
