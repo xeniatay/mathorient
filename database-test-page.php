dc0e79e09f0ec5771f0e90cf5a1d5f9c7ad7729a
Theo Belaire <theo.belaire@gmail.com>
2013-05-06 16:45:48 -0400

    Added database stuff
    With two new pages to test it out.
    database-test-page.php dumps the leaders table,
    and
    leader-protected-page.php is a test to show that we can discrimiate
    based on leaders.

---
+++ database-test-page.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Template Name: Database Test Page
+*/
+?>
+
+
+<?php get_header(); ?>
+
+		<div id="primary">
+			<div id="content" role="main">
+
+<?php
+
+global $wpdb;
+$table_name = $wpdb->prefix . "leaders";
+
+$leaders = $wpdb->get_results(
+    "SELECT *
+    FROM $table_name
+    "
+);
+
+foreach ( $leaders as $leader) 
+{
+    echo $leader->userid;
+    echo "<br />";
+}
+?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
