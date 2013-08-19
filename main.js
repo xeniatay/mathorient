88eebb6289ac2c11f6a815d4320dea4ee3096063
Theo Belaire <theo.belaire@gmail.com>
2013-08-19 15:07:45 -0400

    Commiting style changes before changing other stuff

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $( document ).ready( function() {
     /* Set container height for background hack */
-    $('#content-container').height( $(document).height() );
+    $('#content-container').height( getDocHeight() + 70 );
+    $('.container-fluid').height( getDocHeight() + 70 );
 
     /* Mobile nav flyout */
     $('#nav-toggle').click( function() {
@@ -8,3 +9,14 @@ $( document ).ready( function() {
         $('.site-sidebar').toggleClass('show-nav');
     });
 });
+
+function getDocHeight() {
+
+  var D = document;
+  return Math.max(
+    D.body.scrollHeight, D.documentElement.scrollHeight,
+    D.body.offsetHeight, D.documentElement.offsetHeight,
+    D.body.clientHeight, D.documentElement.clientHeight
+  );
+
+}
